3f39097c916c02d081fbf5673fc7cab5b780506b
Micah Geisel <micah@botandrose.com>
2011-08-30 14:13:36 -0700

    refactor out duplication in db tasks.

--- lib/bard/rake/db.rb
+++ lib/bard/rake/db.rb
@@ -1,33 +1,30 @@
 namespace :db do
   desc "Dump the current database to db/data.sql"
   task :dump => :environment do
-    config = ActiveRecord::Base.configurations[RAILS_ENV || 'development']
-    filepath  = "db/data.sql"
     mysqldump = `which mysqldump`.strip
-    options   =  "-e -u #{config['username']}"
-    options   += " -p'#{config['password']}'" if config['password']
-    options   += " -h #{config['host']}"      if config['host']
-    options   += " -S #{config['socket']}"    if config['socket']
-
-    raise RuntimeError, "I only work with mysql." unless config['adapter'].starts_with? 'mysql'
     raise RuntimeError, "Cannot find mysqldump." if mysqldump.blank?
-    
-    `#{mysqldump} #{options} #{config['database']} > #{filepath}`
+    sh "#{mysqldump} -e #{mysql_options} > #{file_path}"
   end
 
   desc "Load the db/data.sql data into the current database."
-  task :load => :environment do
-    config = ActiveRecord::Base.configurations[RAILS_ENV || 'development']
-    Rake::Task["db:drop"].invoke
-    Rake::Task["db:create"].invoke
+  task :load => ["db:drop", "db:create"] do
     mysql = `which mysql`.strip
-    options  = " -u#{config['username']}"
-    options += " -p#{config['password']}" if config['password']
-    options += " -h #{config['host']}"    if config['host']
-
-    raise RuntimeError, "I only work with mysql." unless config['adapter'].include? 'mysql'
     raise RuntimeError, "Cannot find mysql." if mysql.blank?
+    sh "#{mysql} #{mysql_options} < #{file_path}"
+  end
 
-    sh "#{mysql} #{options} '#{config["database"]}' < db/data.sql"
+  def file_path
+    "db/data.sql"
+  end
+
+  def mysql_options
+    config = ActiveRecord::Base.configurations[Rails.env || "development"]
+    raise RuntimeError, "I only work with mysql." unless config["adapter"].starts_with? "mysql"
+
+    options =  " -u #{config["username"]}"
+    options += " -p'#{config["password"]}'" if config["password"]
+    options += " -h #{config["host"]}"      if config["host"]
+    options += " -S #{config["socket"]}"    if config["socket"]
+    options += " '#{config["database"]}'"
   end
 end
