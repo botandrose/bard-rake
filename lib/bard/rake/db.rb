94edb1103bcde8381fb00fb825cb786fd7971d06
Micah Geisel <micah@botandrose.com>
2012-01-09 17:39:49 -0800

    fix rake deprecation warning.

--- lib/bard/rake/db.rb
+++ lib/bard/rake/db.rb
@@ -24,47 +24,55 @@ module BardRake
   end
 
   class Postgresql
-    def self.dump
-      pg_dump = `which pg_dump`.strip
-      raise RuntimeError, "Cannot find pg_dump." if pg_dump.blank?
-      sh "#{pg_dump} -f#{FILE_PATH} #{database}"
-    end
+    class << self
+      include Rake::DSL
 
-    def self.load
-      psql = `which psql`.strip
-      raise RuntimeError, "Cannot find psql." if psql.blank?
-      sh "#{psql} -q -d#{database} -f#{FILE_PATH}"
-    end
+      def dump
+        pg_dump = `which pg_dump`.strip
+        raise RuntimeError, "Cannot find pg_dump." if pg_dump.blank?
+        sh "#{pg_dump} -f#{FILE_PATH} #{database}"
+      end
 
-    private
+      def load
+        psql = `which psql`.strip
+        raise RuntimeError, "Cannot find psql." if psql.blank?
+        sh "#{psql} -q -d#{database} -f#{FILE_PATH}"
+      end
 
-    def self.database
-      BardRake.database_config["database"]
+      private
+
+      def database
+        BardRake.database_config["database"]
+      end
     end
   end
 
   class Mysql
-    def self.dump
-      mysqldump = `which mysqldump`.strip
-      raise RuntimeError, "Cannot find mysqldump." if mysqldump.blank?
-      sh "#{mysqldump} -e #{mysql_options} > #{FILE_PATH}"
-    end
+    class << self
+      include Rake::DSL
 
-    def self.load
-      mysql = `which mysql`.strip
-      raise RuntimeError, "Cannot find mysql." if mysql.blank?
-      sh "#{mysql} #{mysql_options} < #{FILE_PATH}"
-    end
+      def dump
+        mysqldump = `which mysqldump`.strip
+        raise RuntimeError, "Cannot find mysqldump." if mysqldump.blank?
+        sh "#{mysqldump} -e #{mysql_options} > #{FILE_PATH}"
+      end
 
-    private
-    
-    def self.mysql_options
-      config = BardRake.database_config
-      options =  " -u #{config["username"]}"
-      options += " -p'#{config["password"]}'" if config["password"]
-      options += " -h #{config["host"]}"      if config["host"]
-      options += " -S #{config["socket"]}"    if config["socket"]
-      options += " '#{config["database"]}'"
+      def load
+        mysql = `which mysql`.strip
+        raise RuntimeError, "Cannot find mysql." if mysql.blank?
+        sh "#{mysql} #{mysql_options} < #{FILE_PATH}"
+      end
+
+      private
+      
+      def mysql_options
+        config = BardRake.database_config
+        options =  " -u #{config["username"]}"
+        options += " -p'#{config["password"]}'" if config["password"]
+        options += " -h #{config["host"]}"      if config["host"]
+        options += " -S #{config["socket"]}"    if config["socket"]
+        options += " '#{config["database"]}'"
+      end
     end
   end
 
