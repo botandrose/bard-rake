002a34bc65289c06fabb9dcf99ad33d551bba74a
Micah Geisel <micah@botandrose.com>
2013-03-15 14:08:20 -0700

    add db:migrate:all and db:rollback:all. migrate all on bootstrap.

--- lib/bard/rake/bootstrap.rb
+++ lib/bard/rake/bootstrap.rb
@@ -6,8 +6,36 @@ end
 desc "Bootstrap project"
 task :bootstrap => "bootstrap:files" do
   invoke_task_if_exists "db:create"
-  invoke_task_if_exists "db:migrate"
+  Rake::Task["db:migrate:all"].invoke if Rake::Task.task_defined?("db:migrate")
   Rake::Task["restart"].invoke
+end
+
+namespace :db do
+  namespace :migrate do
+    task :all => [:load_config, :rails_env] do
+      configs_for_environment.each do |config|
+        ActiveRecord::Base.establish_connection(config)
+        ActiveRecord::Migration.verbose = ENV["VERBOSE"] ? ENV["VERBOSE"] == "true" : true
+        ActiveRecord::Migrator.migrate(ActiveRecord::Migrator.migrations_paths, ENV["VERSION"] ? ENV["VERSION"].to_i : nil) do |migration|
+          ENV["SCOPE"].blank? || (ENV["SCOPE"] == migration.scope)
+        end
+      end
+
+      Rake::Task["db:_dump"].invoke
+    end
+  end
+
+  namespace :rollback do
+    task :all => [:load_config, :rails_env] do
+      configs_for_environment.each do |config|
+        ActiveRecord::Base.establish_connection(config)
+        step = ENV['STEP'] ? ENV['STEP'].to_i : 1
+        ActiveRecord::Migrator.rollback(ActiveRecord::Migrator.migrations_paths, step)
+      end
+
+      Rake::Task["db:_dump"].invoke
+    end
+  end
 end
 
 namespace :bootstrap do
