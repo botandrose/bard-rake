a185f97fa7c1970bd19284ad9694662067fe894c
Micah Geisel <micah@botandrose.com>
2013-06-23 16:07:52 -0700

    db migrate sanity rails 4 compat.

--- lib/bard/rake/db_migrate_sanity.rb
+++ lib/bard/rake/db_migrate_sanity.rb
@@ -1,8 +1,9 @@
 if defined?(ActiveRecord)
   namespace :db do
     namespace :migrate do
-      task :all => [:load_config, :rails_env] do
-        configs_for_environment.each do |config|
+      task :all => [:load_config] do
+        invoke_task_if_exists :rails_env
+        run_in_all_environments do |config|
           ActiveRecord::Base.establish_connection(config)
           ActiveRecord::Migration.verbose = ENV["VERBOSE"] ? ENV["VERBOSE"] == "true" : true
           ActiveRecord::Migrator.migrate(ActiveRecord::Migrator.migrations_paths, ENV["VERSION"] ? ENV["VERSION"].to_i : nil) do |migration|
@@ -15,14 +16,23 @@ if defined?(ActiveRecord)
     end
 
     namespace :rollback do
-      task :all => [:load_config, :rails_env] do
-        configs_for_environment.each do |config|
+      task :all => [:load_config] do
+        invoke_task_if_exists :rails_env
+        run_in_all_environments do |config|
           ActiveRecord::Base.establish_connection(config)
           step = ENV['STEP'] ? ENV['STEP'].to_i : 1
           ActiveRecord::Migrator.rollback(ActiveRecord::Migrator.migrations_paths, step)
         end
 
         Rake::Task["db:_dump"].invoke
+      end
+    end
+
+    def run_in_all_environments &block
+      if defined?(ActiveRecord::Tasks::DatabaseTasks)
+        ActiveRecord::Tasks::DatabaseTasks.send :each_local_configuration, &block
+      else
+        configs_for_environment.each &block
       end
     end
   end
