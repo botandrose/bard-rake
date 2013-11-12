5ccf447a8ed705924dc62c72585363eba77505fd
Micah Geisel <micah@botandrose.com>
2013-11-12 15:38:26 -0800

    use bundler for gemspec management instead of jeweler.

--- lib/bard/rake.rb
+++ lib/bard/rake.rb
@@ -6,3 +6,6 @@ require 'bard/rake/testing'
 def invoke_task_if_exists task_name
   Rake::Task[task_name].invoke if Rake::Task.task_defined? task_name
 end
+
+module BardRake
+end
---
+++ lib/bard/rake/version.rb
@@ -0,0 +1,4 @@
+module BardRake
+  VERSION = "0.11.0"
+end
+
