4d1ea133216f487689091a895a705d8e82637831
Micah Geisel <micah@botandrose.com>
2017-07-28 15:39:15 -0700

    greatly simplify bootstrap and db tasks.

--- lib/bard/rake/bootstrap.rb
+++ lib/bard/rake/bootstrap.rb
@@ -1,31 +1,16 @@
 task :restart do
   system "touch tmp/restart.txt"
+  system "if which passenger-config >/dev/null; then passenger-config restart-app `pwd`; fi"
 end
 
 desc "Bootstrap project"
-task :bootstrap => "bootstrap:files" do
-  invoke_task_if_exists "db:create:all"
-  invoke_task_if_exists "db:migrate:all"
+task :bootstrap do
+  system "cp config/database.sample.yml config/database.yml" unless File.exist?('config/database.yml') or !File.exist?('config/database.sample.yml')
+  invoke_task_if_exists "db:create"
+  invoke_task_if_exists "db:migrate"
+  if ENV["RAILS_ENV"] == "production"
+    invoke_task_if_exists "assets:precompile"
+  end
   Rake::Task["restart"].invoke
 end
 
-namespace :bootstrap do
-  desc "Bootstrap project to run in production"
-  task :production => "bootstrap:files" do
-    invoke_task_if_exists "db:create"
-    invoke_task_if_exists "db:migrate"
-
-    invoke_task_if_exists "assets:precompile"
-    invoke_task_if_exists "bootstrap:production:post"
-    Rake::Task["restart"].invoke
-  end
-
-  namespace :production do
-    task :post do
-    end
-  end
-
-  task :files do
-    system "cp config/database.sample.yml config/database.yml" unless File.exist?('config/database.yml') or !File.exist?('config/database.sample.yml')
-  end
-end
--- lib/bard/rake/db_multiple_environment_sanity.rb
+++ lib/bard/rake/db_multiple_environment_sanity.rb
@@ -1,60 +1,28 @@
 if defined?(ActiveRecord)
   namespace :db do
-    namespace :migrate do
-      task :all => [:load_config] do
-        invoke_task_if_exists :rails_env
-        run_in_all_environments do |config|
-          ActiveRecord::Base.establish_connection(config)
-          ActiveRecord::Migration.verbose = ENV["VERBOSE"] ? ENV["VERBOSE"] == "true" : true
-          ActiveRecord::Migrator.migrate(ActiveRecord::Migrator.migrations_paths, ENV["VERSION"] ? ENV["VERSION"].to_i : nil) do |migration|
-            ENV["SCOPE"].blank? || (ENV["SCOPE"] == migration.scope)
-          end
-        end
+    def production?
+      ENV["RAILS_ENV"] == "production"
+    end
 
-        Rake::Task["db:_dump"].invoke
+    def parallel?
+      !!ENV["TEST_ENV_NUMBER"]
+    end
+
+    task :create do
+      unless production? or parallel?
+        invoke_task_if_exists "parallel:create"
       end
     end
 
-    namespace :rollback do
-      task :all => [:load_config] do
-        invoke_task_if_exists :rails_env
-        run_in_all_environments do |config|
-          ActiveRecord::Base.establish_connection(config)
-          step = ENV['STEP'] ? ENV['STEP'].to_i : 1
-          ActiveRecord::Migrator.rollback(ActiveRecord::Migrator.migrations_paths, step)
-        end
-
-        Rake::Task["db:_dump"].invoke
+    task :migrate do
+      unless production? or parallel?
+        invoke_task_if_exists "parallel:migrate"
       end
     end
 
-    def run_in_all_environments &block
-      if defined?(ActiveRecord::Tasks::DatabaseTasks)
-        ActiveRecord::Tasks::DatabaseTasks.send :each_local_configuration, &block
-      else
-        configs_for_environment.each &block
-      end
-    end
-
-    namespace :drop do
-      task :current => [:load_config] do
-        if defined?(ActiveRecord::Tasks::DatabaseTasks)
-          config = ActiveRecord::Tasks::DatabaseTasks.current_config
-          ActiveRecord::Tasks::DatabaseTasks.drop config
-        else
-          drop_database current_config
-        end
-      end
-    end
-
-    namespace :create do
-      task :current => [:load_config] do
-        if defined?(ActiveRecord::Tasks::DatabaseTasks)
-          config = ActiveRecord::Tasks::DatabaseTasks.current_config
-          ActiveRecord::Tasks::DatabaseTasks.create config
-        else
-          create_database current_config
-        end
+    task :rollback do
+      unless production? or parallel?
+        invoke_task_if_exists "parallel:rollback"
       end
     end
   end
