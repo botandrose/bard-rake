9376a34792cc6ec6a32136c95bbfb51440eb36b3
Micah Geisel <micah@botandrose.com>
2019-09-21 14:59:12 -0700

    add db:rollback:all.

--- lib/bard/rake/db_multiple_environment_sanity.rb
+++ lib/bard/rake/db_multiple_environment_sanity.rb
@@ -1,59 +1,87 @@
 if defined?(ActiveRecord)
   namespace :db do
     namespace :create do
-      task :all => [:load_config] do
-        invoke_task_if_exists :rails_env
-        run_in_all_environments do |config|
-          ActiveRecord::Base.establish_connection(config)
-          ActiveRecord::Tasks::DatabaseTasks.create config
-        end
-
-        if includes_test_environment? && !parallel?
-          invoke_task_if_exists "parallel:create"
+      task :all => :load_config do
+        run_in_all_environments do |config, env|
+          if env == "test" && Rake::Task.task_defined?("parallel:create")
+            Rake::Task["parallel:create"].invoke
+          else
+            ActiveRecord::Tasks::DatabaseTasks.create config
+          end
         end
       end
     end
 
     namespace :drop do
-      task :all => [:load_config] do
-        invoke_task_if_exists :rails_env
-        run_in_all_environments do |config|
-          ActiveRecord::Base.establish_connection(config)
-          ActiveRecord::Tasks::DatabaseTasks.drop config
-        end
-
-        if includes_test_environment? && !parallel?
-          invoke_task_if_exists "parallel:drop"
+      task :all => :load_config do
+        run_in_all_environments do |config, env|
+          if env == "test" && Rake::Task.task_defined?("parallel:drop")
+            Rake::Task["parallel:drop"].invoke
+          else
+            ActiveRecord::Tasks::DatabaseTasks.drop config
+          end
         end
       end
     end
 
     namespace :migrate do
-      task :all => [:load_config] do
-        invoke_task_if_exists :rails_env
-        run_in_all_environments do |config|
-          ActiveRecord::Base.establish_connection(config)
-          ActiveRecord::Tasks::DatabaseTasks.migrate
+      task :all => :load_config do
+        run_in_all_environments do |config, env|
+          if env == "test" && Rake::Task.task_defined?("parallel:migrate")
+            Rake::Task["parallel:migrate"].invoke
+          else
+            ActiveRecord::Tasks::DatabaseTasks.migrate
+          end
         end
+      end
+    end
 
-        if includes_test_environment? && !parallel?
-          invoke_task_if_exists "parallel:migrate"
+    namespace :rollback do
+      task :all => :load_config do
+        run_in_all_environments do |config, env|
+          if env == "test" && Rake::Task.task_defined?("parallel:rollback")
+            Rake::Task["parallel:rollback"].invoke
+          else
+            ActiveRecord::Base.connection.migration_context.rollback(1)
+          end
         end
-
-        Rake::Task["db:_dump"].invoke
       end
     end
 
     def run_in_all_environments &block
-      if defined?(ActiveRecord::Tasks::DatabaseTasks)
-        ActiveRecord::Tasks::DatabaseTasks.send :each_local_configuration, &block
-      else
-        configs_for_environment.each &block
+      invoke_task_if_exists :rails_env
+
+      whitelist = %w(development test staging production)
+      modified_databases = Set.new
+
+      ActiveRecord::Base.configurations.configs_for.each do |db_config|
+        env = db_config.env_name
+        configuration = db_config.config
+        database = configuration["database"]
+
+        next unless database
+        next unless whitelist.include?(env)
+        next if modified_databases.include?(database)
+
+        if local_database?(configuration)
+          ActiveRecord::Base.establish_connection configuration
+          block.call configuration, env
+        else
+          $stderr.puts "This task only modifies local databases. #{database} is on a remote host."
+        end
+
+        modified_databases << database
       end
+
+      Rake::Task["db:_dump"].invoke
     end
 
-    def includes_test_environment?
-      %w(development test).include?(rails_env)
+    def local_database?(configuration)
+      configuration["host"].blank? || LOCAL_HOSTS.include?(configuration["host"])
+    end
+
+    def test_environment?
+      rails_env == "test"
     end
 
     def rails_env
@@ -62,10 +90,6 @@ if defined?(ActiveRecord)
       else
         ENV["RAILS_ENV"]
       end
-    end
-
-    def parallel?
-      !!ENV["TEST_ENV_NUMBER"]
     end
   end
 end
