d406821494bf8e437ca99d6f79749bac66e2b2ee
Micah Geisel <micah@botandrose.com>
2022-08-01 16:06:32 +0200

    drop and recreate ONLY the current database.

--- lib/bard/rake/db_dump_load.rb
+++ lib/bard/rake/db_dump_load.rb
@@ -7,35 +7,16 @@ namespace :db do
   end
 
   desc "Load the db/data.sql data into the current database."
-  task :load => ["db/data.sql", "db:drop:current", "db:create:current"] do
+  task :load => ["db:drop:current", "db:create:current"] do
     Backhoe.load
   end
-end
 
-# FIXME is this necessary? Why can't we just rely on RAILS_ENV?
-if defined?(ActiveRecord)
-  namespace :db do
-    namespace :create do
-      task :current => :load_config do
-        if ActiveRecord::Tasks::DatabaseTasks.respond_to?(:current_config)
-          config = ActiveRecord::Tasks::DatabaseTasks.current_config
-          ActiveRecord::Tasks::DatabaseTasks.create config
-        else
-          ActiveRecord::Tasks::DatabaseTasks.create_current Rails.env
-        end
-      end
-    end
+  task "drop:current" => :environment do
+    ActiveRecord::Tasks::DatabaseTasks.drop Rails.env.to_sym
+  end
 
-    namespace :drop do
-      task :current => :load_config do
-        if ActiveRecord::Tasks::DatabaseTasks.respond_to?(:current_config)
-          config = ActiveRecord::Tasks::DatabaseTasks.current_config
-          ActiveRecord::Tasks::DatabaseTasks.drop config
-        else
-          ActiveRecord::Tasks::DatabaseTasks.drop_current Rails.env
-        end
-      end
-    end
+  task "create:current" => :environment do
+    ActiveRecord::Tasks::DatabaseTasks.create Rails.env.to_sym
   end
 end
 
